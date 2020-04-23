03de9b425c1746a1feb6cbf123e87ce8c74da75a
razmy <razmyasdf@gmail.com>
2020-04-24 01:10:59 +0530

    data.sql file added resources->data.sql to populate the database on app startup. Closes #5

--- src/main/java/com/sixonix/recipe/models/UnitOfMeasure.java
+++ src/main/java/com/sixonix/recipe/models/UnitOfMeasure.java
@@ -12,4 +12,20 @@ public class UnitOfMeasure {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String description;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
---
+++ src/main/resources/data.sql
@@ -0,0 +1,13 @@
+insert into category(description) values ('American')
+insert into category(description) values ('Italian')
+insert into category(description) values ('Mexican')
+insert into category(description) values ('Sri Lankan')
+insert into category(description) values ('Indian')
+insert into category(description) values ('Chinese')
+insert into category(description) values ('Arabian')
+
+insert into unit_of_measure (description) values ('Teaspoon')
+insert into unit_of_measure (description) values ('Tablespoon')
+insert into unit_of_measure (description) values ('Cup')
+insert into unit_of_measure (description) values ('Pinch')
+insert into unit_of_measure (description) values ('Ounce')
